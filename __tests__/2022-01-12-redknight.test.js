44ce2910805f3564ddb51ea0a4bc9d862f0a5565
Balazs Bencs <balazs.bencs@ibm.com>
2022-01-12 16:04:23 +0100

    initialized repo with node & jest

---
+++ __tests__/2022-01-12-redknight.test.js
@@ -0,0 +1,14 @@
+const { expect } = require('chai');
+const { redknight } = require('../src/2022-01-12-redknight');
+
+describe('User story 1', () => {
+  it('', () => {
+    expect(redknight()).equal(undefined);
+  });
+  it.skip('', () => {});
+});
+
+describe('User story 2', () => {
+  it.skip('', () => {});
+  it.skip('', () => {});
+});
---
+++ src/2022-01-12-redknight.js
@@ -0,0 +1,3 @@
+const redknight = () => {};
+
+module.exports = { redknight };
